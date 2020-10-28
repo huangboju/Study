ddc1150b0953b1e8f929830729644986a6fc3ff7
huangboju <bhuang@xiaohongshu.com>
2020-10-28 23:08:56 +0800

    删除没用代码

--- wanlezu/wanlezu/Classes/Models/UserModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  User.swift
-//  wanlezu
-//
-//  Created by 伯驹 黄 on 2017/6/16.
-//  Copyright © 2017年 伯驹 黄. All rights reserved.
-//
-
-class UserModel: BaseModel {}
--- wanlezu/wanlezu/Libertine/Extensions/UICollectionReusableView+Extension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright © 2016年 xiAo_Ju. All rights reserved.
-//
-
-extension UICollectionReusableView {
-    class var cellID: String {
-        return String(describing: self)
-    }
-}
